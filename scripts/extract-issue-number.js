9255cae201912ac6973996681cd8176f4b4354fa
Chandra Kiran <chandrakiran.vish@gmail.com>
2021-07-11 18:05:15 +0530

    Update extract-issue-number.js

--- scripts/extract-issue-number.js
+++ scripts/extract-issue-number.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
 
 const issueUrl = process.argv[2];
+console.log('issueUrl', issueUrl);
 process.stdout.write(issueUrl.substring(issueUrl.lastIndexOf('/') + 1, issueUrl.length));
